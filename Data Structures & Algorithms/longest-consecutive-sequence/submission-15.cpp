24454c2f61593cd7f42db9d932898fba440af38f
Aymen-AitOuraies <140119301+Aymen-AitOuraies@users.noreply.github.com>
2026-07-15 19:24:17 +0100

    Add: longest-consecutive-sequence - submission-15

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-15.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto x : nums)
+            s.insert(x);
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            if(s.size() != 0)
+                count++;
+            int num = nums[i];
+            if(s.count(num - 1))
+                continue;
+
+            while(s.count(num + 1)) {
+                num = num + 1;
+                count++;
+            }
+            if(count > res)
+                res = count;
+        }
+        return res;
+    }
+};
