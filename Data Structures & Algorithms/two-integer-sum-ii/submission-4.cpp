0ccc35dd4807d0e0507aa40efc711e0906d32968
Aymen-AitOuraies <140119301+Aymen-AitOuraies@users.noreply.github.com>
2026-07-16 21:20:24 +0100

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for(int i = 0; i < numbers.size() - 1; i++) {
+            int n = target - numbers[i];
+            auto it = lower_bound(numbers.begin() +1, numbers.end(), n);
+            if(it != numbers.end() && *it == n) {
+                int idx = it - numbers.begin();
+                return {i + 1, idx + 1};
+            }
+        }
+        return {};
+    }
+};
