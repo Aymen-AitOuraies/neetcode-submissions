b2a613ee9a6679053f91bbd8d562bc8b03c1261f
Aymen-AitOuraies <140119301+Aymen-AitOuraies@users.noreply.github.com>
2026-07-18 18:12:28 +0100

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> pre;
+        vector<int> suf(n);
+        pre.push_back(height[0]);
+        suf[n - 1] = height[n - 1];
+        for(int i = 1; i < n; i++) {
+            pre.push_back(max(pre[i - 1], height[i]));
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suf[i] = max(suf[i + 1], height[i]);
+        }
+        int water = 0;
+        for(int i = 0; i < n; i++) {
+            water += min(pre[i], suf[i]) - height[i];
+        }
+        return water; 
+    }
+};
